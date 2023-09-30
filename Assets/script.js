dce38e6c29c874273d4e54382493994609ae3483
nchoin <139597297+nchoin@users.noreply.github.com>
2023-09-30 15:20:47 -0400

    updated links from Index to js and css. began adding window prompts for password generator including notation for why I'm doing each step.

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// const types = {
+//   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+//   lowerCase: "abcdefghijklmnopqrstuvwxyz",
+//   numbers: "0123456789",
+//   symbols: "!@#$%*()_+=?><"
+// }
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,6 +15,27 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  //creating prompts for inputs 
+  var passwordLength = prompt("How many characters would you like to use for your password?");
+  var specChar = prompt("Click OK to use special characters in your password like !@#$%*()_+=?>< ");
+  var numbers = prompt("Click OK to use numbers in your password.");
+  var lowerCase = prompt("Click OK to use lowercase letters in your password.");
+  var upperCase = prompt("Click OK to use uppercase letters in your password.");
 
+
+
+
+
+
+
+
+
+
+
+
+
+  return //generated password
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
