1cae4e96a0346a3c91333e440dcb8907a15548a5
nchoin <139597297+nchoin@users.noreply.github.com>
2023-09-30 17:03:04 -0400

    updated javascript from prompt to confirm in initial phase of the code.

--- Assets/script.js
+++ Assets/script.js
@@ -21,13 +21,18 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specChar = ["!", "@", "#", "$", "%", "*", "(", ")", "_", "+", "=", "?", ">", "<"];
 
 function generatePassword() {
-  //creating prompts and names for inputs 
-  var passwordLength = prompt("How many characters would you like to use for your password?");
-  var specCharInput = prompt("Click OK to use special characters in your password like !@#$%*()_+=?>< ");
-  var numbersInput = prompt("Click OK to use numbers in your password.");
-  var lowerCaseInput = prompt("Click OK to use lowercase letters in your password.");
-  var upperCaseInput = prompt("Click OK to use uppercase letters in your password.");
-
+  //Prompt to determine length of password
+  var passwordLength = prompt("How many characters would you like to use for your password? Choose between 8 to 128 characters.");
+  //redirect to retry if length does not match criteria
+  if (passwordLength< 8 || passwordLength > 128) {
+    alert("Your password needs to be between 8 and 128 characters in length. Please try again.")
+    return;
+  } else {
+  var specCharInput = confirm("Click OK to use special characters in your password like !@#$%*()_+=?>< ");
+  var numbersInput = confirm("Click OK to use numbers in your password.");
+  var lowerCaseInput = confirm("Click OK to use lowercase letters in your password.");
+  var upperCaseInput = confirm("Click OK to use uppercase letters in your password.");
+  }
   //get random index from array of options and pull them from the arrays above
 var lowerCaseIndex = Math.floor(Math.random() * lowerCase.length);
 var lowerChoices = lowerCase[lowerCaseIndex];
